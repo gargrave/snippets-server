70b9fad9b190196d3c40f54e8ad6da8672c2f34f
Gabe Hargrave <rinkrinkerfink@gmail.com>
2016-12-29 14:30:15 -0800

    Add Surge to CORS whitelist

--- main/settings/staging.py
+++ main/settings/staging.py
@@ -11,3 +11,7 @@ DATABASES = {'default': dj_database_url.config()}
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+CORS_ORIGIN_WHITELIST = (
+    'https://whispering-motion.surge.sh',
+)
