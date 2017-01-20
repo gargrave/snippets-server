c8f3c577337291e3e8fdcbe369d3003f882448b2
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-01-20 08:22:46 -0800

    Update staging CORS whitelist

--- main/settings/staging.py
+++ main/settings/staging.py
@@ -13,8 +13,9 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 CORS_ORIGIN_WHITELIST = (
-    'https://whispering-motion.surge.sh',
     'https://www.snippets-app.com',
     'www.snippets-app.com',
     'snippets-app.com',
+    'https://gargrave-snippets-dev.netlify.com',
+    'gargrave-snippets-dev.netlify.com',
 )
