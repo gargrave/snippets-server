bfdf76810cc93c08d402c3f79c05ad74e626c523
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-01-20 09:02:23 -0800

    Update static files path

--- main/settings/base.py
+++ main/settings/base.py
@@ -100,7 +100,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
