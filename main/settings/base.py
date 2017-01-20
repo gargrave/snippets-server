51d0c4e10a2be31d4ed21499166d06f9886e2427
Gabe Hargrave <rinkrinkerfink@gmail.com>
2017-01-20 09:51:52 -0800

    Update ALLOWED_HOSTS setting

--- main/settings/base.py
+++ main/settings/base.py
@@ -90,7 +90,10 @@ USE_TZ = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'snippets-dev.herokuapp.com',
+    'snippets-prod.herokuapp.com'
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
